168effac0e2d88b505cbd5b85b416b34c228a0af
RakkoDaisuki <251zs00908@student.zen.ac.jp>
2026-02-11 16:39:28 +0900

    Update kadai.js

--- kadai.js
+++ kadai.js
@@ -4,20 +4,24 @@ let headding1 = document.getElementById('headding1');
  function rotateHeadding1() {
    degree = degree + 5;
    headding1.style.transform = 'rotateX(' + degree + 'deg)';
- }//タテ回転です
+ }
+//タテ回転です
  setInterval(rotateHeadding1, 40);
  let tatoe = document.getElementById('tatoe');
 let degree1 = 0;
  function rotatetatoe() {
    degree1 = degree1 + 6;
    tatoe.style.transform = 'rotateY(' + degree1 + 'deg)';
- }//横回転です
+ }
+//横回転です
  setInterval(rotatetatoe, 40);
  let mosimo = document.getElementById('mosimo');
  let degree2 = 0;
  function rotatemosimo() {
    degree2 = degree2 + 4;
    mosimo.style.transform = 'rotateX(' + degree2 + 'deg)';
- }//タテ回転です
+ }
+//タテ回転です
 
  setInterval(rotatemosimo,50);
+
